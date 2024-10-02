b76cba5bd55494dc60a14ba9ac3148d35a1d46da
Vlad <vladsevruk8@gmail.com>
2024-10-02 19:23:31 +0300

    4

--- 4/2.py
+++ 4/2.py
@@ -1,2 +1,2 @@
-squares_of_even_numbers = [x**2 for x in range(0, 21) if x % 2 == 0]
-print(squares_of_even_numbers)
+sqr= [x**2 for x in range(0, 21) if x % 2 == 0]
+print(sqr)
--- 4/3.py
+++ 4/3.py
@@ -1,10 +1,10 @@
-numbers = [4, 11, 15, 7, 20, 3, 12]
-max_number = numbers[0]
+num = [4, 11, 15, 7, 20, 3, 12]
+max_num = num[0]
 i = 1
 
-while i < len(numbers):
-    if numbers[i] > max_number:
-        max_number = numbers[i]
+while i < len(num):
+    if num[i] > max_num:
+        max_num = num[i]
     i += 1
 
-print(f"Максимальное число в списке: {max_number}")
+print(f"Максимальное число в списке: {max_num}")
